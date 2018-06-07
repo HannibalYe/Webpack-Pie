573c8cdd35868be8903d4e854729f98f27a627a3
jason.zhenchi.ye <jason.zhenchi.ye@accenture.com>
2018-06-07 11:22:21 +0800

    add

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,3 @@
 //入口文件
-document.getElementById("title").innerHTML="hello this is yezhenchi's first react project";
+import css from './css/index.css'; //css-loader（css处理） style-loader（url路径处理,挂载js上） 打包
+document.getElementById("title").innerHTML="hello this is yezhenchi's first react project！！";
